a3163cc1af23f2eaa7605e2d8974e94bf289946c
Fabian Jeske <s150574@th-ab.de>
2020-11-14 11:16:11 +0100

    added starbucks

--- get_stock_data.py
+++ get_stock_data.py
@@ -3,14 +3,16 @@ from create_stock import Stock
 
 
 def main():
-    tickers = ['APC.DE', 'AMZ.DE']
+    tickers = ['APC.DE', 'AMZ.DE', 'SRB.DE']
 
     #creates an object of a stock
     apple = Stock(ticker=tickers[0], stock_name='Apple', start_date='2019-01-14', end_date=str(datetime.now().strftime('%Y-%m-%d')))
-    amazon = Stock(ticker=tickers[0], stock_name='Amazon', start_date='2019-01-14', end_date=str(datetime.now().strftime('%Y-%m-%d')))
+    amazon = Stock(ticker=tickers[1], stock_name='Amazon', start_date='2019-01-14', end_date=str(datetime.now().strftime('%Y-%m-%d')))
+    starbucks = Stock(ticker=tickers[2], stock_name='Starbucks', start_date='2019-01-14', end_date=str(datetime.now().strftime('%Y-%m-%d')))
 
     apple.get_data()
     amazon.get_data()
+    starbucks.get_data()
 
 
 
